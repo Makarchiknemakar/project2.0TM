45782b011de4883a6ac9b211475537c65f36fbc5
Makarchiknemakar <163634628+Makarchiknemakar@users.noreply.github.com>
2025-05-26 19:02:48 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,49 +1,55 @@
+ // ПОВНИЙ JAVASCRIPT
 let answers = document.querySelectorAll('.answer');
-let question  = document.querySelector('.question');
+let question = document.querySelector('.question');
 let result = document.querySelector('.result');
 let start = document.querySelector('.start');
 let container = document.querySelector('.container');
 let mainContainer = document.querySelector('.main-container');
 
-let signs = ['-','+','/','*'];
-
-start.addEventListener('click', function(){
-    container.style.display = "flex";
-    mainContainer.style.display = "none";
-
-    currentQuest.display();
-});
+let signs = ['-', '+', '/', '*'];
+let currentQuest;
+let rightCount = 0;
+let total = 0;
+let questionCount = 0;
 
 class Question {
     constructor(a, b) {
         this.a = a || this.randNumber(-100, 100);
-        this.b = b || this.randNumber(0, 100);
+        this.b = b || this.randNumber(1, 100); // уникнення 0 для ділення
         this.sign = this.randSign();
-        this.b = this.sign === '/' && this.b === 0 ? 1 : this.b; // уникнення ділення на 0
+        this.b = this.sign === '/' && this.b === 0 ? 1 : this.b;
         this.question = `${this.a} ${this.sign} ${this.b}`;
         this.correct = this.makeCorrect();
-        this.answers = [
-            this.randNumber(this.correct - 10, this.correct + 10),
-            this.randNumber(this.correct - 10, this.correct + 10),
-            this.randNumber(this.correct - 10, this.correct + 10),
-            this.randNumber(this.correct - 10, this.correct + 10),
-            this.correct
-        ];
+        this.answers = this.generateAnswers();
         this.shuffle(this.answers);
+    }
+
+    generateAnswers() {
+        let set = new Set();
+        set.add(this.correct);
+
+        while (set.size < 5) {
+            let fake = this.randNumber(this.correct - 10, this.correct + 10);
+            if (this.sign === '/') fake = Number(fake.toFixed(2));
+            set.add(fake);
+        }
+
+        return Array.from(set);
     }
 
     display() {
         question.innerHTML = this.question;
         for (let i = 0; i < answers.length; i++) {
             answers[i].innerHTML = this.answers[i];
+            answers[i].style.backgroundColor = '#ff7f50'; // скидання кольору
         }
     }
 
     shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) { 
-            let j = Math.floor(Math.random() * (i + 1));  
-            [array[i], array[j]] = [array[j], array[i]]; 
-        } 
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
     }
 
     randNumber(min, max) {
@@ -51,75 +57,66 @@ class Question {
     }
 
     randSign() {
-        let x = this.randNumber(0, 3);
-        return signs[x];
+        return signs[Math.floor(Math.random() * signs.length)];
     }
 
     makeCorrect() {
         switch (this.sign) {
-            case '-':
-                return this.a - this.b;
-            case '+':
-                return this.a + this.b;
-            case '*':
-                return this.a * this.b;
-            case '/':
-                return Number((this.a / this.b).toFixed(2));
+            case '-': return this.a - this.b;
+            case '+': return this.a + this.b;
+            case '*': return this.a * this.b;
+            case '/': return Number((this.a / this.b).toFixed(2));
         }
     }
 }
 
-let currentQuest = new Question();
-
-let rightCount = 0;
-let total = 0;
-let questionCount = 0;
-
 function testResult() {
     mainContainer.style.display = "flex";
     container.style.display = "none";
+    result.style.display = "block";
     result.innerHTML = `
-        Ви дали: ${rightCount} правильних відповідей із: ${total}
+        Ви дали: ${rightCount} правильних відповідей із ${total}.<br>
         Точність: ${(rightCount / total * 100).toFixed(2)}%
     `;
 }
 
-for (let i = 0; i < answers.length; i++) {
-    answers[i].addEventListener('click', function() {
-        if (answers[i].innerHTML == currentQuest.correct) {
-            answers[i].style.backgroundColor = "#00ff00";
-            anime({
-                targets: answers[i],
-                delay: 500,
-                backgroundColor: '#ff7f50', 
-                duration: 500, 
-                easing: 'linear' 
-            });
+// Старт гри
+start.addEventListener('click', function () {
+    rightCount = 0;
+    total = 0;
+    questionCount = 0;
+    result.style.display = "none";
+
+    container.style.display = "flex";
+    mainContainer.style.display = "none";
+
+    currentQuest = new Question();
+    currentQuest.display();
+});
+
+// Обробка відповіді
+answers.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let userAnswer = Number(btn.innerHTML);
+        let correctAnswer = Number(currentQuest.correct.toFixed(2));
+
+        if (userAnswer.toFixed(2) == correctAnswer.toFixed(2)) {
+            btn.style.backgroundColor = "#00ff00";
             rightCount++;
         } else {
-            answers[i].style.backgroundColor = "#ff0000";
-            anime({
-                targets: answers[i],
-                delay: 500,
-                backgroundColor: '#ff7f50', 
-                duration: 500,
-                easing: 'linear' 
-            });
+            btn.style.backgroundColor = "#ff0000";
         }
 
         total++;
         questionCount++;
 
-        if (questionCount >= 12) {
-            testResult();
-        } else {
-            // Скидання кольору кнопок перед наступним питанням
-            for (let btn of answers) {
-                btn.style.backgroundColor = '#ff7f50';
+        setTimeout(() => {
+            if (questionCount >= 12) {
+                testResult();
+            } else {
+                currentQuest = new Question();
+                currentQuest.display();
             }
-
-            currentQuest = new Question();
-            currentQuest.display();
-        }
+        }, 600);
     });
-}
+});
